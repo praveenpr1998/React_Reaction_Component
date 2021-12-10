4fefb6cc32f2f745e724eb3eb9c1ac2e0d1aabbc
praveen <praveenraj.thiagaraju@conversight.ai>
2021-12-10 23:42:29 +0530

    Changes

--- src/components/AllReactions/AllReactions.js
+++ src/components/AllReactions/AllReactions.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from "react";
+import React, { useState, useEffect } from "react";
 import "./AllReactions.scss";
 import { Tabs } from "antd";
 import ALL_CONSTANTS from "../../Saga/Constants/Constants";
